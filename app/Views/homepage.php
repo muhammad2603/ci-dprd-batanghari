e2d563933b5b6ec9128eabcf71ec16910544b0c2
Muhammad Fattahillah. Mz <fattahillahmuhammad48@gmail.com>
2026-02-07 01:59:55 +0700

    Instalasi Fonts dan Tailwind CSS serta menambahkan halaman homepage

---
+++ app/Views/homepage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda | Dewan Perwakilan Rakyat Daerah Kabupaten Batang Hari</title>
+    <link rel="stylesheet" href="<?= base_url('assets/css/tailwindcss/tailwind-utils.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/fonts.css') ?>">
+</head>
+
+<body>
+
+    <h1 class="font-title">Hello, World!</h1>
+
+    <p class="font-regular">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas nam velit tempore repellat doloribus distinctio culpa ea aspernatur, esse aut, amet laboriosam consequatur laudantium quos, ipsa asperiores voluptates quam enim.</p>
+
+</body>
+
+</html>
